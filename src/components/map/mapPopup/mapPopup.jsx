eec9cf09986bd8cbc34a15f4c2bc92f7759d0baa
JONGHYUK LEE <dlwhd990@gmail.com>
2022-02-08 14:00:03 +0900

    1. 장소 팝업 리뷰 디자인 수정 2. 장소 팝업 주변 관광지 UI구성

--- src/components/map/mapPopup/mapPopup.jsx
+++ src/components/map/mapPopup/mapPopup.jsx
@@ -5,6 +5,7 @@ import ReactStars from "react-rating-stars-component";
 import axios from "axios";
 import HelmetComponent from "../../helmetComponent/helmetComponent";
 import MapPopupReviewItem from "./mapPopupReviewItem/mapPopupReviewItem";
+import MapRecommendSlick from "../../slick/mapRecommendSlick/mapRecommendSlick";
 
 const MapPopup = ({
   popupValue,
@@ -15,6 +16,74 @@ const MapPopup = ({
   reviewList,
   totalReviewCount,
 }) => {
+  // 주변 관광지 임시데이터
+  const [chabak, setChabak] = useState([
+    {
+      idx: 0,
+      image:
+        "https://d2ur7st6jjikze.cloudfront.net/offer_photos/68004/592612_medium_1636105470.jpg?1636105470",
+      title: "스코틀랜드 아일랜드 8일간의 여행",
+      type: "투어패키지",
+      price: 20000,
+    },
+    {
+      idx: 1,
+      image:
+        "https://d2ur7st6jjikze.cloudfront.net/offer_photos/42185/262013_medium_1536304187.jpg?1536304187",
+      title: "제주도 차박",
+      type: "패키지",
+      price: 20000,
+    },
+    {
+      idx: 2,
+      image:
+        "https://d2ur7st6jjikze.cloudfront.net/offer_photos/7511/296672_medium_1544173662.jpg?1544173662",
+      title: "바티칸투어",
+      type: "입장권",
+      price: 20000,
+    },
+    {
+      idx: 3,
+      image:
+        "https://d2ur7st6jjikze.cloudfront.net/offer_photos/70816/595376_medium_1638331669.jpg?1638331669",
+      title: "롯데월드 입장권",
+      type: "교통편",
+      price: 20000,
+    },
+    {
+      idx: 4,
+      image:
+        "https://d2ur7st6jjikze.cloudfront.net/offer_photos/42185/262013_medium_1536304187.jpg?1536304187",
+      title: "올 오브 피렌체",
+      type: "투어패키지",
+      price: 20000,
+    },
+    {
+      idx: 5,
+      image:
+        "https://d2ur7st6jjikze.cloudfront.net/offer_photos/68004/592612_medium_1636105470.jpg?1636105470",
+      title: "제주도차박",
+      type: "교통편",
+      price: 20000,
+    },
+    {
+      idx: 6,
+      image:
+        "https://d2ur7st6jjikze.cloudfront.net/offer_photos/53556/592873_medium_1636329887.jpg?1636329887",
+      title: "제주 아쿠아플라넷 입장권",
+      type: "입장권",
+      price: 20000,
+    },
+    {
+      idx: 7,
+      image:
+        "https://d2ur7st6jjikze.cloudfront.net/offer_photos/100746/548935_medium_1608107406.jpg?1608107406",
+      title:
+        "[바티칸 공인가이드] 이것이 베테랑 클라쓰! 에너지 넘치는 바티칸 반일 투어!",
+      type: "투어패키지",
+      price: 20000,
+    },
+  ]);
   const [instaUrl, setInstaUrl] = useState(null);
   const [blogUrl, setBlogUrl] = useState(null);
   const [spotData, setSpotData] = useState(null);
@@ -221,11 +290,46 @@ const MapPopup = ({
         </div>
         <div className={styles.nearby_container}>
           <p className={styles.nearby_title}>주변 관광지</p>
+          <div className={styles.nearby_main}>
+            {chabak.map((item) => (
+              <div
+                key={item.idx ? item.idx : item.product_id}
+                className={styles.item}
+              >
+                <img
+                  src={item.image}
+                  alt="thumbnail"
+                  className={styles.thumbnail}
+                />
+                <div className={styles.data_container}>
+                  <p className={styles.name}>{item.title}</p>
+                  <div className={styles.star_container}>
+                    <ReactStars
+                      count={5}
+                      edit={false}
+                      size={14}
+                      value={4}
+                      activeColor="#000000"
+                      isHalf={true}
+                      emptyIcon={<i className="fas fa-paw"></i>}
+                      halfIcon={<i className="fas fa-paw"></i>}
+                      filledIcon={<i className="fas fa-paw"></i>}
+                    />
+                    <p className={styles.rating_text}>4.0점</p>
+                  </div>
+                  <p className={styles.price}>{`${item.price.toLocaleString(
+                    "ko-KR"
+                  )}원`}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
         <div className={styles.review_container}>
-          <p className={styles.review_title}>{`리뷰 (${
-            totalReviewCount || 0
-          })`}</p>
+          <p className={styles.review_title}>
+            리뷰{" "}
+            <span className={styles.review_count}>{totalReviewCount || 0}</span>
+          </p>
           <div className={styles.review_main}>
             {reviewList &&
               reviewList.map((review) => (
--- src/components/map/mapPopup/mapPopupReviewItem/mapPopupReviewItem.jsx
+++ src/components/map/mapPopup/mapPopupReviewItem/mapPopupReviewItem.jsx
@@ -3,25 +3,45 @@ import styles from "./mapPopupReviewItem.module.css";
 import ReactStars from "react-rating-stars-component";
 
 const MapPopupReviewItem = ({ review }) => {
+  // <div className={styles.star_container}>
+  //       <div className={styles.star}>
+  //         <ReactStars
+  //           count={5}
+  //           edit={false}
+  //           size={16}
+  //           value={review.rating}
+  //           activeColor="#000000"
+  //           isHalf={true}
+  //           emptyIcon={<i className="fas fa-paw"></i>}
+  //           halfIcon={<i className="fas fa-paw"></i>}
+  //           filledIcon={<i className="fas fa-paw"></i>}
+  //         />
+  //       </div>
+  //       <p className={styles.name}>{review.user.email.slice(0, 3) + "***"}</p>
+  //     </div>
+  //     <p className={styles.content}>{review.content}</p>
   return (
     <div className={styles.item}>
-      <div className={styles.star_container}>
-        <div className={styles.star}>
-          <ReactStars
-            count={5}
-            edit={false}
-            size={16}
-            value={review.rating}
-            activeColor="#000000"
-            isHalf={true}
-            emptyIcon={<i className="fas fa-paw"></i>}
-            halfIcon={<i className="fas fa-paw"></i>}
-            filledIcon={<i className="fas fa-paw"></i>}
-          />
-        </div>
+      <div className={styles.name_and_rate}>
         <p className={styles.name}>{review.user.email.slice(0, 3) + "***"}</p>
+        <div className={styles.rate_container}>
+          <div className={styles.rate_star}>
+            <ReactStars
+              count={5}
+              edit={false}
+              size={17}
+              value={review.rating}
+              activeColor="#000000"
+              isHalf={true}
+              emptyIcon={<i className="fas fa-paw"></i>}
+              halfIcon={<i className="fas fa-paw"></i>}
+              filledIcon={<i className="fas fa-paw"></i>}
+            />
+          </div>
+          <p className={styles.rate_text}>{`${review.rating}.0점`}</p>
+        </div>
       </div>
-      <p className={styles.content}>{review.content}</p>
+      <div className={styles.content}>{review.content}</div>
     </div>
   );
 };
